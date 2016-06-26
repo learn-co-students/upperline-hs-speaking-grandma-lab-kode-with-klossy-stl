db29f26841a49ae0ea2353d687affbcc44889be4
Theresa Fister <tfister97@gmail.com>
2016-06-26 22:44:08 +0000

    Done.

--- lib/grandma.rb
+++ lib/grandma.rb
@@ -1,2 +1,9 @@
 # Write your code here!
-
+def speak_to_grandma(words)
+  if words.upcase
+  return "NO, NOT SINCE 1938!"
+else
+  return "HUH?! SPEAK UP, SONNY!"
+end
+end
+puts speak_to_grandma("hello")
